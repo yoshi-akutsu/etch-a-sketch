57925838ea55ba02bc75691f8c3e81a3f78e65c2
Yoshi Akutsu <akutsu.yoshi@gmail.com>
2020-01-08 15:10:03 -0500

    styled divs appropriately and added a css color switcher.

--- etch.js
+++ etch.js
@@ -2,19 +2,24 @@ function createGrid(side) {
     //creates the columns
     for (let i = 0; i < side; i++){
         const bigSquare = document.createElement("div");
-        bigSquare.classList.add("bigSquare", "flex-container")
+        bigSquare.classList.add("bigSquare")
         //creates the rows
         for (let j = 0; j < side; j++){
             const littleSquare = document.createElement("div");
-            littleSquare.classList.add("littleSquare")
-            littleSquare.textContent = "box";
+            littleSquare.classList.add("littleSquare");
+            littleSquare.classList.remove("drawn");
             bigSquare.appendChild(littleSquare);
-
         }
         container.appendChild(bigSquare);
+        //adds event listener for littleSquares each time
+        let lilSquares = document.querySelectorAll("div.littleSquare");
 
+        lilSquares.forEach((lilSquare) => {
+        lilSquare.addEventListener('mouseover', (e) => {
+            colorChange(event.target);
+            });
+        });
     }
-    
 }
 
 function killTheKids(div){
@@ -24,16 +29,32 @@ function killTheKids(div){
     return
 }
 
-const container = document.querySelector("#container");
+function colorChange(square) {
+    square.classList.add("drawn");
+}
 
-createGrid(16);
-
+function checkSides(side){
+    if(side === null){
+        return;
+    }
+    else if (side == '') {
+        killTheKids(container);
+        createGrid(16)
+    }
+    else {
+        killTheKids(container);
+        createGrid(side);
+    }
+}
 
 
 //button at the top to "shake the etch-a-sketch"
 const button = document.querySelector("#reset");
 button.addEventListener("click", (e) => {
-    killTheKids(container);
     let side = prompt("Please enter the side-density:");
-    createGrid(side);
+    checkSides(side);
 });
+
+//Creates initial grid
+const container = document.querySelector("#container");
+createGrid(16);
